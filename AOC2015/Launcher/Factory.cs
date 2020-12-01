c3fe0f7c7de0abb1b2ad1ef3618285be446d3e06
FUNCTOR99 <dan.hooper@gmail.com>
2020-11-30 23:18:37 -0700

    adding 2020 day 1

--- AOC2015/Launcher/Factory.cs
+++ AOC2015/Launcher/Factory.cs
@@ -269,6 +269,19 @@ namespace AOC2015
                     }
                     break;
 
+                case 202001:
+                    switch (part)
+                    {
+                        case 1:
+                            IInput inputp1 = CreateInput(inputType, "Inputs/2020_d01p01_input.txt");
+                            return new AOC2020Day01Part1(inputp1.GetInput(), standardMessages);
+
+                        case 2:
+                            IInput inputp2 = CreateInput(inputType, "Inputs/2020_d01p01_input.txt");
+                            return new AOC2020Day01Part2(inputp2.GetInput(), standardMessages);
+                    }
+                    break;
+
                 default:
                     break;
             }
