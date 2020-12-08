ba0f7b615aa0b3692576298097d34b4127d06ab9
FUNCTOR99 <dan.hooper@gmail.com>
2020-12-07 23:04:28 -0700

    Cleaned up 2020 Day 8

--- AOC2015/2020/AOC2020Day08/AOC2020Day08Part1.cs
+++ AOC2015/2020/AOC2020Day08/AOC2020Day08Part1.cs
@@ -11,18 +11,11 @@ namespace AOC2015
 
         protected override String DoSolve(String[] input)
         {
-            int result = 0;
-
             List<string[]> operations = new List<string[]>();
-
-
 
             foreach (String line in input)
             {
                 operations.Add(line.Split(' '));
-
-
-
             }
 
             HandheldGameConsole console = new HandheldGameConsole(operations);
@@ -31,50 +24,7 @@ namespace AOC2015
 
             int acc = console.Execute(out instructionRepeated);
 
-            //int acc = 0;
-
-            //int currentOperation = 0;
-            //bool instructionRepeated = false;
-
-            //List<int> executedInstructions = new List<int>();
-
-            //while (!instructionRepeated)
-            //{
-            //    if (executedInstructions.Contains(currentOperation))
-            //    {
-            //        instructionRepeated = true;
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        executedInstructions.Add(currentOperation);
-
-            //        switch (operations[currentOperation][0])
-            //        {
-            //            case "acc":
-
-            //                acc = acc + Convert.ToInt32(operations[currentOperation][1]);                            
-            //                currentOperation++;
-            //                break;
-
-            //            case "jmp":
-            //                currentOperation = currentOperation + Convert.ToInt32(operations[currentOperation][1]);
-            //                break;
-
-            //            case "nop":
-            //                currentOperation++;
-            //                break;
-
-            //        }
-            //    }
-            //}
-
-
-
-
-
-            return $"Result { acc }.";            
-
+            return $"Result { acc }.";        
         }
     }
 }
